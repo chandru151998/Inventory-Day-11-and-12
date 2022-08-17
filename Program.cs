14a684af6de33360fac4e4431535dcc10754628c
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-17 13:49:32 +0530

    Problem 3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace EmployeeWage
+﻿namespace StocksInventory
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Inventory Management Program");
+            StockPortfolio stockPortfolio = new StockPortfolio();
+            string filepath = ".\\..\\..\\..\\JSONDATA.json";
+
+            Stocks inventory = stockPortfolio.ReadFromFile(filepath);
+
+            Console.WriteLine("Total Stocks value: " + stockPortfolio.GetInventoryTotalValue());
+
+            Console.WriteLine("Total Textile stocks price: " + stockPortfolio.m_inventoryFactory.GetTextilePrice());
+            Console.WriteLine("Total Paper stocks price: " + stockPortfolio.m_inventoryFactory.GetPaperPrice());
+            Console.WriteLine("Total Packaging stocks price: " + stockPortfolio.m_inventoryFactory.GetPackagingPrice());
         }
     }
 }
