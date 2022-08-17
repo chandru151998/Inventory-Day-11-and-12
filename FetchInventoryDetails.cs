194e0d37e34bdc67ec0ac2060734bb8f60125b62
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-17 13:43:52 +0530

    Problem 2

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class FetchInventoryDetails
+    {
+        public InventoryFactory ReadRice(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryFactory>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+        public InventoryFactory ReadWheat(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryFactory>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+        public InventoryFactory ReadPulse(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryFactory>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
